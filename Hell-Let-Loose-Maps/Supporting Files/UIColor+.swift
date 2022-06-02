fadf86927a4ca96a84b004f31c6d349b27d397ae
Micah <micahburnside@protonamil.com>
2022-06-02 03:31:31 -0400

    adds UIColor+ extension for custom colors

---
+++ Hell-Let-Loose-Maps/Supporting Files/UIColor+.swift
@@ -0,0 +1,21 @@
+//
+//  UIColor+.swift
+//  HLL Maps
+//
+//  Created by Micah Burnside on 6/2/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    class func getFriendlyBlue() -> UIColor{
+        return UIColor(red:67, green:150 ,blue:206 , alpha:1.00)
+    }
+    class func getEnemyRed() -> UIColor{
+        return UIColor(red:67, green:150 ,blue:206 , alpha:14)
+    }
+    class func getNeutralWhite() -> UIColor{
+        return UIColor(red: 255, green: 255, blue: 255, alpha: 28)
+    }
+}
