7ecb7c2e8ddcd9f2d50cf37730cf84ed4c44cd13
Micah <micahburnside@protonamil.com>
2022-09-24 14:21:01 -0400

    V1 Complete

--- Hell-Let-Loose-Maps/View Controllers/BaseLayerViewControllers/StalingradBaseLayerViewController.swift
+++ Hell-Let-Loose-Maps/View Controllers/BaseLayerViewControllers/StalingradBaseLayerViewController.swift
@@ -278,7 +278,7 @@ class StalingradBaseLayerViewController: BaseViewController {
             let pointInView = sender.location(in: imageView)
            
             // 2
-            var scale = min(scrollView.zoomScale * 2, scrollView.maximumZoomScale)
+            let scale = min(scrollView.zoomScale * 2, scrollView.maximumZoomScale)
             
             // 3
             if scale != scrollView.zoomScale {
