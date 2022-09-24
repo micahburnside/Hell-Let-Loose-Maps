46760ce3edf14a32577848d8edcf836aa2bd64a2
Micah <micahburnside@protonamil.com>
2022-09-24 19:42:46 -0400

    sets statusBar text to white in light and dark mode

--- Hell-Let-Loose-Maps/Supporting Files/AppDelegate.swift
+++ Hell-Let-Loose-Maps/Supporting Files/AppDelegate.swift
@@ -11,11 +11,12 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-//            UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.darkContent
+
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
         return true
     }
 
